d4403115d1e130f02cb3b41e63b2b3cd2ee6a2aa
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2017-10-05 12:51:49 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,4 +1,4 @@
-<?// PHP Data Objects(PDO) Sample Code:
+<?PHP
 try {
     $conn = new PDO("sqlsrv:server = tcp:saplin.database.windows.net,1433; Database = БазаSQL", "saplin64", "Amerika1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
