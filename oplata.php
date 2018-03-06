6347e07774c380df28e470c1fc40d68f9987adfa
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-03-06 17:45:43 +0400

    Create oplata.php

---
+++ oplata.php
@@ -0,0 +1,10 @@
+<html>
+<h3>Оплата</h3>
+<h4>Введите сумму</h4>
+<input type="text" 
+name="name" id="name"/></br>
+<input type="Submit" 
+name="submit1" value="Menu" />
+    <input type="Submit" 
+name="submit" value="ok" />
+<html>
