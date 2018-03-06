06bcd792771ad3769e48b0e9c8a20c508149ec2f
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-03-06 17:56:44 +0400

    Create vozvrat.php

---
+++ vozvrat.php
@@ -0,0 +1,10 @@
+<html>
+<h3>Возврат</h3>
+<h4>Введите сумму(руб):</h4>
+<input type="text" 
+name="name" id="name"/></br>
+<input type="Submit" 
+name="submit1" value="Menu" />
+    <input type="Submit" 
+name="submit" value="ok" />
+<html>
