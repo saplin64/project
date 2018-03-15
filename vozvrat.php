fd50d6f15070bba024c3b2a56a4eb872aea33723
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-03-15 10:46:08 +0400

    Update vozvrat.php

--- vozvrat.php
+++ vozvrat.php
@@ -6,8 +6,6 @@ name="name" id="name"/></br>
 <h5>Введите номер карты</h5>
 <input type="text" 
 name="name12" id="name12"/></br>
-<input type="Submit" 
-name="submit1" value="Menu" />
     <input type="Submit" 
 name="submit" value="ok" />
 <html>
