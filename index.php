506b0ca2345ae72fd65d18faeab27c0d52fa7218
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-03-15 10:49:29 +0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 <ul class="menu-main">
   <li><a href="oplata.php" class="current">Оплата</a></li>
   <li><a href="vozvrat.php">Возврат</a></li>
-  <li><a href="">Сверка итогов</a></li>
+  <li><a href="sverka.php">Сверка итогов</a></li>
 </ul>
 <html>
