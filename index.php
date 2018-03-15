e82c4f6776618260c243c3c8e692cc9d3aeea5ba
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-03-15 11:53:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <html>
-      <h3>Меню</h3>
+      <h3>Выбор операции</h3>
 <ul class="menu-main">
   <li><a href="oplata.php" class="current">Оплата</a></li>
   <li><a href="vozvrat.php">Возврат</a></li>
   <li><a href="sverka.php">Сверка итогов</a></li>
+  <li><a href="sverka.php">Повтор чека</a></li>
+  <li><a href="sverka.php"></a></li>
 </ul>
 <html>
