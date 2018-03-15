0dd2730e23fd55126c566617e00897f834f29fd2
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-03-15 11:57:50 +0300

    Create karta.php

---
+++ karta.php
@@ -0,0 +1,11 @@
+<html>
+ <h3>Оплата</h3>
+Номер Карты  <input type="text" 
+name="name" id="name"/></br>
+     Пароль  <input type="text" 
+name="email" id="email"/></br>
+MONTH/YEAR  <input type="text" 
+name="name" id="name"/></br>
+    <input type="Submit" 
+name="submit" value="Добавить" />
+</html>
