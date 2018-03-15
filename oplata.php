5fddf9c8c7dd6cd501c2dcf7e0e7f880448144cd
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-03-15 11:54:39 +0300

    Update oplata.php

--- oplata.php
+++ oplata.php
@@ -4,8 +4,6 @@
 name="name" id="name"/></br>
      Пароль  <input type="text" 
 name="email" id="email"/></br>
-    CCV  <input type="text" 
-name="name" id="name"/></br>
 MONTH/YEAR  <input type="text" 
 name="name" id="name"/></br>
     <input type="Submit" 
