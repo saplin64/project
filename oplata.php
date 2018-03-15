98d44c81c379cb30fa0354f15f310635417563b1
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-03-15 10:40:12 +0400

    Update oplata.php

--- oplata.php
+++ oplata.php
@@ -1,7 +1,12 @@
 <html>
-<h3>Оплата</h3>
-<h4>Введите сумму(руб):</h4>
-<input type="text" 
+      <h3>Оплата</h3>
+      Номер Карты  <input type="text" 
+name="name" id="name"/></br>
+     Пароль  <input type="text" 
+name="email" id="email"/></br>
+    CCV  <input type="text" 
+name="name" id="name"/></br>
+MONTH/YEAR  <input type="text" 
 name="name" id="name"/></br>
 <input type="Submit" 
 name="submit1" value="Menu" />
