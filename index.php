cb4195e6070005572e636c1332d58cf03d33ab1b
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-03-15 10:42:29 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <html>
-      <h3>Оплата</h3>
-      Номер Карты  <input type="text" 
-name="name" id="name"/></br>
-     Пароль  <input type="text" 
-name="email" id="email"/></br>
-    CCV  <input type="text" 
-name="name" id="name"/></br>
-MONTH/YEAR  <input type="text" 
-name="name" id="name"/></br>
-<input type="Submit" 
-name="submit1" value="Menu" />
-    <input type="Submit" 
-name="submit" value="ok" />
+      <h3>Меню</h3>
+<ul class="menu-main">
+  <li><a href="" class="current">Work</a></li>
+  <li><a href="">About</a></li>
+  <li><a href="">Blog</a></li>
+  <li><a href="">Contact</a></li>
+</ul>
 <html>
