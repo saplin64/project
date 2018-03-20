2d27f3f6e6f280292158c1a2b8fd9f39ec5583ac
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-03-20 10:06:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
+<!DOCTYPE html>
 <html>
-      <h3>Выбор операции</h3>
-<ul class="menu-main">
-  <li><a href="oplata.php" class="current">Оплата</a></li>
-  <li><a href="vozvrat.php">Возврат</a></li>
-  <li><a href="sverka.php">Сверка итогов</a></li>
-  <li><a href="karta.php">Добавить карту</a></li>
-</ul>
-<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Выбор операции/title>
+  </head>
+  <body>
+    <div class="container">
+          <h3>Выбор операции</h3><hr>
+          <div><a href="oplata.php" class="current">Оплата</a></div>
+          <div><a href="vozvrat.php">Возврат</a>/div>
+          <div><a href="sverka.php">Сверка итогов</a></div>
+          <div><a href="karta.php">Добавить карту</a></div>
+
+    </div>
+  </body>
+</html>
