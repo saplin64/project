b029db9eaf233a24c7037ba3d4eb4541806d61c5
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-04-08 20:27:11 +0400

    Update kart.php

--- kart.php
+++ kart.php
@@ -2,13 +2,13 @@
   try {
     $conn = new PDO("sqlsrv:server = tcp:saplin.database.windows.net,1433; Database = БазаSQL", "saplin64", "Amerika1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql ="CREATE TABLE `Kart` (
-`№Kart` int(16) NOT NULL,
-`Password` int(4) NOT NULL,
-`Balance` int(5) NOT NULL,
-`Year` int(2) NOT NULL,
-`Month` int(2) NOT NULL,
-PRIMARY KEY (`№Kart`)
+    $sql ="CREATE TABLE Kart (
+№Kart int NOT NULL,
+Password int NOT NULL,
+Balance int NOT NULL,
+Year int NOT NULL,
+Month int NOT NULL,
+PRIMARY KEY (№Kart)
 )";
     $conn->quary(sql);
     
