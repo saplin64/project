43d9017ed83487681e9e4739170b7ebdf0dc56c9
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-04-08 20:47:31 +0400

    Update kart.php

--- kart.php
+++ kart.php
@@ -2,16 +2,17 @@
   try {
     $conn = new PDO("sqlsrv:server = tcp:saplin.database.windows.net,1433; Database = БазаSQL", "saplin64", "Amerika1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql ="CREATE TABLE Kart1 (
-NKart INT NOT NULL
-PRIMARY KEY (NKart),
-Password INT NOT NULL,
-Balance INT NOT NULL,
-Year INT NOT NULL,
-Month INT NOT NULL
+   $sql = "CREATE TABLE Card (
+  	id INT NOT NULL IDENTITY(1,1)
+	PRIMARY KEY (Ncard),
+	Ncard varchar(30) NOT NULL,
+	Balance INT NOT NULL,
+  Year INT NOT NULL,
+ Month INT NOT NULL,
+	Password varchar(30) NOT NULL
+	
 )";
-    $conn->quary($sql);
-    
+    $conn->query($sql);    
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
