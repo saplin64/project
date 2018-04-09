4e3595706347671bb53a0cf92b00135c08b564c5
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-04-09 10:34:19 +0300

    Create operat.php

---
+++ operat.php
@@ -0,0 +1,21 @@
+<?php
+try {
+    $conn = new PDO("sqlsrv:server = tcp:vol2.database.windows.net,1433; Database = BD", "Volun", "Simpsons1");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+  $sql = "CREATE TABLE Operat (
+  id INT NOT NULL IDENTITY(1,1)
+  PRIMARY KEY(id),
+	Ncard varchar(30) NOT NULL,
+	Sum INT NOT NULL,
+	Operation varchar(30) NOT NULL,
+  	date DATE
+	
+)";
+    $conn->query($sql);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+?>
