9d8388c13a6989deb28cf3b768655e7489a0842b
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-04-10 22:43:04 +0400

    Update sverka.php

--- sverka.php
+++ sverka.php
@@ -1,9 +1,21 @@
-<html>
-<h3>Сверка итогов</h3>
-Период  <input type="text" 
-name="name" id="name"/></br>
-<input type="Submit" 
-name="submit" value="Меню" />
-<input type="Submit" 
-name="submit" value="ok" />
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Сверка итогов</title>
+  </head>
+  <body>
+    <div class="container-2">
+        <legend>Сверка итогов</legend><hr>
+        <div class="input-box">
+          <label for="name" style="font-size: 1.4em;">Период</label><br>
+          <input type="text" name="name" id="name">
+        </div>
+        <div class="submit-box">
+          <input type="submit" name="menu" value="Меню">
+          <input type="submit" name="okey" value="ОК">
+        </div>
+    </div>
+  </body>
 </html>
