15fa56ad7315d941f1fcc5b2286d5796d9154972
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-04-10 22:43:24 +0400

    Update vozvrat.php

--- vozvrat.php
+++ vozvrat.php
@@ -1,13 +1,23 @@
-<html>
-<h3>Возврат</h3>
-<h4>Введите сумму(руб):</h4>
-<input type="text" 
-name="name" id="name"/></br>
-<h5>Введите номер карты</h5>
-<input type="text" 
-name="name12" id="name12"/></br>
-<input type="Submit" 
-name="submit" value="Меню" />
-    <input type="Submit" 
-name="submit" value="ok" />
-<html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Возврат</title>
+  </head>
+  <body>
+    <div class="container-2">
+        <legend>Возврат</legend><hr>
+        <div class="input-box">
+          <label for="name"><p style="width: 150px;">Введите сумму(руб)</p></label>
+          <input type="text" name="name" id="name">
+          <label for="name12"><p style="width: 150px;">Введите номер карты</p></label>
+          <input type="text" name="name12" id="name12">
+        </div>
+        <div class="submit-box">
+          <input type="submit" name="menu" value="Меню">
+          <input type="submit" name="okey" value="ОК">
+        </div>
+    </div>
+  </body>
+</html>
