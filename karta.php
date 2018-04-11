6be69771ae21133f1719807a57936df4a4a9a84d
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-04-11 16:04:20 +0300

    Update karta.php

--- karta.php
+++ karta.php
@@ -9,57 +9,30 @@
   <h3>Информация о организации</h3>
  <hr>
 <div>Организация: Суши-бар ФЕНИКС</div>
-<div>Баланс:</div>
-<div>Счет организации:</div>
+<div>Счет организации:  <?php
+ 
+ 
+ $sql_select4 = "Select Schet From Org Where Name ='Phenix'";
+ 	$k4 = $conn->query($sql_select4);
+		$data = $k4->fetchAll();
+    foreach($data as $registrant) {
+	     $S = $registrant['Schet'];	
+    }
+ 
+
+?>
+  </div>
 </html>
 
 <?php
-if(!empty($_POST)) {
 try {
-$karta = $_POST['nomerkart'];
-$pass = $_POST['pass'];
-$motch = $_POST['motch'];
-$year = $_POST['year'];
-$balanc = $_POST['balanc'];
-// Insert data
-$sql_insert =
-"INSERT INTO registration_tbl (№Kart, Password, Balance, Year, Month)
-VALUES (?,?,?,?,?)";
-$stmt = $conn->prepare($sql_insert);
-$stmt->bindValue(1, $karta);
-$stmt->bindValue(2, $pass);
-$stmt->bindValue(3, $balanc);
-$stmt->bindValue(4, $year);
-$stmt->bindValue(5, $motch);
-$stmt->execute();
-}
-catch(Exception $e) {
-die(var_dump($e));
-}
-echo "<h3>Your're registered!</h3>";
-}
-
-$sql_select = "SELECT * FROM Kart";
-$stmt = $conn->query($sql_select);
-$registrants = $stmt->fetchAll();
-if(count($registrants) > 0) {
-echo "<h2>People who are registered:</h2>";
-echo "<table>";
-echo "<tr><th>№Kart</th>";
-echo "<th>Password</th>";
-echo "<th>Month</th>";
-echo "<th>Year</th>";
-echo "<th>Balance</th></tr>";
-foreach($registrants as $registrant) {
-echo "<tr><td>".$registrant['№Kart']."</td>";
-echo "<td>".$registrant['Password']."</td>";
-echo "<td>".$registrant['Month']."</td>";
-echo "<td>".$registrant['Year']."</td>";
-echo "<td>".$registrant['Balance']."</td></tr>";
-}
-echo "</table>";
-} else {
-echo "<h3>No one is currently registered.</h3>";
+   $conn = new PDO("sqlsrv:server = tcp:saplin.database.windows.net,1433; Database = БазаSQL", "saplin64", "Amerika1");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+    }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 ?>
 
