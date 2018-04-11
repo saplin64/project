b7fbe8d32f7c9e69c090531e70636df32ecf1874
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2018-04-11 16:15:39 +0300

    Update karta.php

--- karta.php
+++ karta.php
@@ -1,3 +1,15 @@
+<?php
+try {
+   $conn = new PDO("sqlsrv:server = tcp:saplin.database.windows.net,1433; Database = БазаSQL", "saplin64", "Amerika1");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+    }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+?>
+
 <html>
  <head>
     <meta charset="utf-8">
@@ -17,22 +29,12 @@
 		$data = $k4->fetchAll();
     foreach($data as $registrant) {
 	     $S = $registrant['Schet'];	
+	     echo $S;
     }
- echo $S;
 
 ?>
   </div>
 </html>
 
 <?php
-try {
-   $conn = new PDO("sqlsrv:server = tcp:saplin.database.windows.net,1433; Database = БазаSQL", "saplin64", "Amerika1");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- 
-    }
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-?>
-
+	 ?>
