7bcba420179d53426eba34fa85ed15caa3345067
Alexander Saplin <31986405+saplin64@users.noreply.github.com>
2019-05-11 13:13:03 +0400

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
     <title>Выбор операции</title>
   </head>
   <body>
-    <div class="menu-main">
-          <ul>
+    <ul class="menu-main">
           <li><a href="oplata.php" class="current">Оплата</a></li>
           <li><a href="vozvrat.php">Возврат</a></li>
           <li><a href="sverka.php">Сверка итогов</a></li>
